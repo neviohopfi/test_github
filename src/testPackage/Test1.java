1e984cb61ae5aa55c3fb0d91702f9b5e85a7eb77
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-01-15 10:30:17 +0100

    another test

---
+++ src/testPackage/Test1.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Fortnite ist cool!");
+		System.out.println("Lyon kommt aus Vils");
+		
+	}
+
+}
---
+++ src/testPackage/Test2.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Andreas ist schlau!");
+		System.out.println("Er kommt aus Lähn");
+
+	}
+
+}
