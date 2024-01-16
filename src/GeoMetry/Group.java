29c10f36e3b58353aa449aa658622ad4bc7221fc
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-01-16 09:45:10 +0100

    Create class
    Getter Setter Constructor

---
+++ src/GeoMetry/Group.java
@@ -0,0 +1,23 @@
+package GeoMetry;
+
+import java.util.ArrayList;
+
+public class Group {
+
+	private ArrayList<Shape> shapes;
+
+	public Group(ArrayList<Shape> shapes) {
+		this.shapes = new ArrayList<Shape>();
+	}
+
+	public ArrayList<Shape> getShapes() {
+		return shapes;
+	}
+
+	public void setShapes(ArrayList<Shape> shapes) {
+		this.shapes = shapes;
+	}
+	
+	
+	
+}
