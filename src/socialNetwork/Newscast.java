4be4d206365b7d57d6af623440d726a4b01615aa
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-01-29 11:21:38 +0100

    Getter / Setter + Constructor

--- src/socialNetwork/Newscast.java
+++ src/socialNetwork/Newscast.java
@@ -1,5 +1,30 @@
 package socialNetwork;
 
-public class Newscast {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Newscast extends News{
+
+	private String textMessage;
+
+	public Newscast(String author, LocalDate timestamp, int likes, ArrayList <String> comments, String textMessage) {
+		this.textMessage = textMessage;
+		this.setAuthor(author);
+		this.setTiemstamp(LocalDate.now());
+		this.setLikes(likes);
+		this.setComments(comments);
+		this.textMessage = textMessage;
+	}
+
+	public String getTextMessage() {
+		return textMessage;
+	}
+
+	public void setTextMessage(String textMessage) {
+		this.textMessage = textMessage;
+	}
+	
+	
+	
+	
 }
