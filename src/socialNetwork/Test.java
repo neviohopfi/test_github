2275bdd081e54eadb4dcdec661fc22ac51a4c1fc
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-01-29 11:44:26 +0100

    Created test class + two tests

---
+++ src/socialNetwork/Test.java
@@ -0,0 +1,15 @@
+package socialNetwork;
+
+public class Test {
+
+	public static void main(String[] args) {
+		NewsFeed nf = new NewsFeed();
+		News n = new Newscast("Andreas", "Das ist ein Text");
+		News p = new Photocast("Nevio", "image1.png", "Klassenfoto");
+		
+		nf.add(n);
+		nf.add(p);
+		
+	}
+
+}
