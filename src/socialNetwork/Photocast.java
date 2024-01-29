4895da615c5e6527575981bd2eb9233e30943ef1
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-01-29 11:29:04 +0100

    Getter / Setter + Constructor

--- src/socialNetwork/Photocast.java
+++ src/socialNetwork/Photocast.java
@@ -1,5 +1,22 @@
 package socialNetwork;
 
-public class Photocast {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Photocast extends News{
+
+	private String photoFileName;
+	private String photoHeadline;
+	
+
+	public Photocast(String author, LocalDate timestamp, int likes, ArrayList <String> comments,
+			String photoFileName, String photoHeadline) {
+		this.setAuthor(author);
+		this.setTiemstamp(LocalDate.now());
+		this.setLikes(likes);
+		this.setComments(comments);
+		this.photoFileName = photoFileName;
+		this.photoHeadline = photoHeadline;
+	}
+		
 }
