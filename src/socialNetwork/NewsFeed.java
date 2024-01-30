93cf08041778faf4f0c1e39391d9bc6079574f70
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-01-30 09:15:48 +0100

    added the Method like

--- src/socialNetwork/NewsFeed.java
+++ src/socialNetwork/NewsFeed.java
@@ -45,9 +45,16 @@ public class NewsFeed {
 				System.out.println(n);
 			}
 		}
-		
-		
-	
-	
-		
+				
+		public void like(News n) {
+			for(News ne : this.news) {
+				if(ne.equals(n)) {
+					int likes = ne.getLikes();
+					ne.setLikes(likes++);
+					System.out.println(ne);
+				}else{
+				System.err.println("Fehler: Beitrag nicht vorhanden");
+				 }
+			}
+		}		
 }
