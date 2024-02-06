6b05b1fae6169f011c3d225999c0a442fed99d71
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-02-06 09:21:28 +0100

    Add Getter/Setter, constructor and toString

---
+++ src/socialNetwork/Comment.java
@@ -0,0 +1,41 @@
+package socialNetwork;
+
+public class Comment {
+
+	private String text;
+	private int Likes;
+	
+	
+	public Comment(String text) {
+		this.text = text;
+		this.Likes = 0;
+	}
+
+
+	public String getText() {
+		return text;
+	}
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+
+	public int getLikes() {
+		return Likes;
+	}
+
+
+	public void setLikes(int likes) {
+		Likes = likes;
+	}
+	
+	
+	public String toString() {
+		return "Kommentar: \n" + 
+				"\t Text: " +this.text + "\n" +
+				"\t Anzahl Likes " + this.Likes + "\n";
+	}
+	
+}
