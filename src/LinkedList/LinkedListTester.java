5ebe3c18fa44f185d74f0450436f38ec8503cd9c
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 08:36:44 +0100

    Created Linked List

--- src/LinkedList/LinkedListTester.java
+++ src/LinkedList/LinkedListTester.java
@@ -1,9 +1,13 @@
 package LinkedList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Integer> list = new LinkedList<Integer>();
 
 	}
 
