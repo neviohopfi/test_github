1ea12a75ff13fe8376db56b087d371d03658a6f2
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 09:44:38 +0100

    Tested

--- src/musicalChairs/MusicalChairsGame.java
+++ src/musicalChairs/MusicalChairsGame.java
@@ -4,11 +4,14 @@ public class MusicalChairsGame {
 
 	public static void main(String[] args) {
 		
-		MusicalChair mc1 = new MusicalChair("Lisa", "Susi", "Tom");
+		MusicalChair mc1 = new MusicalChair("Lisa", "Susi", "Tom", "Lukas");
 		System.out.println(mc1);
 		
 		MusicalChair mc2 = new MusicalChair("Madeleine", "Benedikt");
 		System.out.println(mc2);
+		
+		mc1.rotate(2);
+		System.out.println(mc1);
 		
 
 	}
