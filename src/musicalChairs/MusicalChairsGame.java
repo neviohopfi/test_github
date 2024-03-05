550126d1d76b8ba081299489f90edae1b4a0544d
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 09:35:18 +0100

    Added mc2 and mc3

--- src/musicalChairs/MusicalChairsGame.java
+++ src/musicalChairs/MusicalChairsGame.java
@@ -6,6 +6,12 @@ public class MusicalChairsGame {
 		
 		MusicalChair mc1 = new MusicalChair("Lisa", "Susi", "Tom");
 		System.out.println(mc1);
+		
+		MusicalChair mc2 = new MusicalChair("Madeleine", "Benedikt");
+		System.out.println(mc2);
+		
+		MusicalChair mc3 = new MusicalChair();
+		System.out.println(mc3);
 
 	}
 
