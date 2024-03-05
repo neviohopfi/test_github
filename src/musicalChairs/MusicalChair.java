d3ef9aaa14eb576af0e06bc5c517ea993d16877a
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 09:26:03 +0100

    Added if and comments

--- src/musicalChairs/MusicalChair.java
+++ src/musicalChairs/MusicalChair.java
@@ -1,11 +1,22 @@
 package musicalChairs;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MusicalChair {
 
 	private List<String>names;
 
+	public MusicalChair(String... names) {
+		// String... kann beliebig viele Strings übernehmen
+		// Java regelt das intern mit einem Array
+		// dadurch wird length und nicht size verwendet!
+		if(names.length > 0) {
+			this.names = new LinkedList<String>(Arrays.asList(names));
+			
+		}
+	}
 	
 	public List<String> getNames() {
 		return names;
@@ -13,7 +24,5 @@ public class MusicalChair {
 
 	public void setNames(List<String> names) {
 		this.names = names;
-	}
-	
-	
+	}	
 }
