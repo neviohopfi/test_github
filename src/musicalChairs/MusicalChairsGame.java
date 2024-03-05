ef96044bf271a08ddb94f64ccb9baf23e6880058
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 09:32:24 +0100

    Added an object

--- src/musicalChairs/MusicalChairsGame.java
+++ src/musicalChairs/MusicalChairsGame.java
@@ -4,7 +4,8 @@ public class MusicalChairsGame {
 
 	public static void main(String[] args) {
 		
-		
+		MusicalChair mc1 = new MusicalChair("Lisa", "Susi", "Tom");
+		System.out.println(mc1);
 
 	}
 
