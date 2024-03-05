9fdd4badf4bd4234cd70d513e1e2ed458e15caf0
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 08:38:04 +0100

    Added sth to list

--- src/LinkedList/LinkedListTester.java
+++ src/LinkedList/LinkedListTester.java
@@ -8,7 +8,10 @@ public class LinkedListTester {
 	public static void main(String[] args) {
 		
 		List<Integer> list = new LinkedList<Integer>();
-
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
 	}
 
 }
