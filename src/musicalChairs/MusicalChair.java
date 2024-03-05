d23fe4107f05177f9f53e1a1426358050be2854a
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-05 09:19:52 +0100

    created getter and setter

--- src/musicalChairs/MusicalChair.java
+++ src/musicalChairs/MusicalChair.java
@@ -1,7 +1,19 @@
 package musicalChairs;
 
+import java.util.List;
+
 public class MusicalChair {
 
+	private List<String>names;
+
+	
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
 	
 	
 }
