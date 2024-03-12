d9ad85b3aad55d16a8ccc8d6022c8d6c50c2f0aa
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-12 08:39:13 +0100

    finished method and wrote main

--- src/LinkedList/CheeseInserters.java
+++ src/LinkedList/CheeseInserters.java
@@ -1,29 +1,42 @@
 package LinkedList;
 
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class CheeseInserters {
-
+	
+			// private -> Sichtbarkeitsbereich auf Klasse beschränkt
+			// finale -> Variable kann danach nicht mehr verändert werden
+			// static -> abhängig von der Klasse und nicht vom Objekt
 	private static final Pattern vegetables = Pattern.compile(
-			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?"
+			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?|Melanzani"
 			);
 	
 	public static void insertCheeseAroundVegetable(List<String> ingredients) {
-		//Initialisierung Laufvariable (iterator)
-		//Abbruchbedingung (hasNext())
-		//keine Inkrementierung -> passiert im Schleifenkopf
+		// Initialisierung Laufvariable (iterator)
+		// Abbruchbedingung (hasNext())
+		// keine Inkrementierung -> passiert im Schleifenkopf
 		for(ListIterator<String> iterator = ingredients.listIterator(); iterator.hasNext();) {
 			String ingredient = iterator.next();
+			// matcher -> wird verwendet, um ingredient in unserem Muster zu finden
+			// matches() -> für den direkten Vergleich
+			if(vegetables.matcher(ingredient).matches()) {
+				iterator.add("Käse");
+			}
+			
 		}
 	}
 	
 	public static void main(String[] args) {
-		
-		
+		List<String> ingredients = new LinkedList<String>();
+					// addAll -> Kursiv, weil es eine static Methode ist
+		Collections.addAll(ingredients, "Gnocchi", "Paprika", "Tomaten", "Pfeffer", "Melanzani");
+		insertCheeseAroundVegetable(ingredients);
+		System.out.println(ingredients);
 		
 	}
-
 }
