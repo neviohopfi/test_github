738c7c1dd48db418d570fd41939562dbf5aab13a
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-03-12 09:28:09 +0100

    Added constructor

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -7,4 +7,11 @@ public class ArrayQueue {
 	private static int capacity;
 	private static int[] queue;
 	
+	public ArrayQueue(int size) {
+		front = 0;
+		rear = 0;
+		capacity = size;
+		queue = new int[size];
+	}
+	
 }
