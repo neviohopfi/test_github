47850b560696e984a802270ff1fb53075ed94bf0
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-04-09 08:36:13 +0200

    finished task one

---
+++ src/Stack/Aufgabe_1.java
@@ -0,0 +1,22 @@
+package Stack;
+
+import java.util.LinkedList;
+
+public class Aufgabe_1 {
+
+	public static String reverseString(String input) {
+		LinkedList<Character> list = new LinkedList<Character>();
+		char[] reversedList = new char[input.length()];
+		for(int i=0; i<input.length(); i++) {
+			list.push(input.charAt(i));
+		}
+		for(int i = 0; i < input.length(); i++) {
+			reversedList[i]=list.pop();
+		}
+		return String.valueOf(reversedList);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(reverseString("valerija"));
+	}
+}
