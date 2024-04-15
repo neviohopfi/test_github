81fa9c88ad32aecc6b8bc256e3926ec106de8ee3
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-04-15 10:17:47 +0200

    created classes

---
+++ src/Stack/Aufgabe_2.java
@@ -0,0 +1,10 @@
+package Stack;
+
+public class Aufgabe_2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/Stack/Aufgabe_3.java
@@ -0,0 +1,25 @@
+package Stack;
+
+public class Aufgabe_3 {
+
+	public static double calculateTwoNumberss(double num1, double num2) {
+		
+	}
+	
+	
+	public static void calculateUPN(String input) {
+		String [] list = input.split(" ");
+		for(int i = 0; i < list.length; i++) {
+			System.out.println(list[i]);
+		}
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
