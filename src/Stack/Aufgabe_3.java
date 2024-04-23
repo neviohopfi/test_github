d86bb0b69e3923af7fd4cf14ba9d627bcd9378c6
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-04-23 09:08:50 +0200

    finished

--- src/Stack/Aufgabe_3.java
+++ src/Stack/Aufgabe_3.java
@@ -1,25 +1,43 @@
 package Stack;
 
-public class Aufgabe_3 {
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	public static double calculateTwoNumberss(double num1, double num2) {
+public class Aufgabe_3 {
+	
+	public static int calculateUPN(String input) {
+		LinkedList<Integer> stack = new LinkedList<>();
+		// operatorPattern: überprüft auf mathematische Rechenzeichen
+		Pattern operatorPattern = Pattern.compile("[+*-/]");
+		// numericPattern: \\d ... Ziffer [0-9], + ... beliebig viele Ziffern
+		Pattern numericPattern = Pattern.compile("\\d+");
 		
-	}
-	
-	
-	public static void calculateUPN(String input) {
-		String [] list = input.split(" ");
-		for(int i = 0; i < list.length; i++) {
-			System.out.println(list[i]);
+		// \\s+ ... teilt den String auch nach mehreren Leerzeichen
+		for(String token : input.split("\\s+")) {
+			Matcher operatorMatcher = operatorPattern.matcher(token);
+			Matcher numericMatcher = numericPattern.matcher(token);
+			if(numericMatcher.matches()) {
+				stack.push(Integer.parseInt(token));
+			} else if(operatorMatcher.matches()) {
+				int operand1 = stack.pop();
+				int operand2 = stack.pop();
+				int result = 0;
+				switch(token) {
+					case "+": result = operand1 + operand2; break;
+					case "-": result = operand2 -  operand1; break;
+					case "*": result = operand1 * operand2; break;
+					case "/": result = operand2 / operand1; break;
+					default: System.out.println("Falscher Operator");
+				}
+				stack.push(result);
+			}	
 		}
-		
+		return stack.pop();
 	}
-	
-	
 	
 	public static void main(String[] args) {
-		
-
-	}
-
+	    String str = " 5 3 +";
+	    System.out.println(calculateUPN(str));
+    }
 }
