8b6502c9d2d0278e5645583465cbf57d1eee13f6
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-04-23 08:06:45 +0200

    finished

--- src/Stack/Aufgabe_1.java
+++ src/Stack/Aufgabe_1.java
@@ -5,18 +5,19 @@ import java.util.LinkedList;
 public class Aufgabe_1 {
 
 	public static String reverseString(String input) {
-		LinkedList<Character> list = new LinkedList<Character>();
-		char[] reversedList = new char[input.length()];
-		for(int i=0; i<input.length(); i++) {
-			list.push(input.charAt(i));
-		}
-		for(int i = 0; i < input.length(); i++) {
-			reversedList[i]=list.pop();
-		}
-		return String.valueOf(reversedList);
-	}
+		LinkedList<Character> stack = new LinkedList<Character>(); //Character weil es eine Wrapper Klasse ist
+		for(char c : input.toCharArray()){
+		 	stack.push(c);
+		 }
+		 StringBuilder reversedString = new StringBuilder();
+		 while(!stack.isEmpty()) {
+			 reversedString.append(stack.pop());
+		 }
+		 return reversedString.toString();
+	}		
 	
 	public static void main(String[] args) {
-		System.out.println(reverseString("valerija"));
+		System.out.println("Hello World!");
+		System.out.println(reverseString("Andi Arbeit"));
 	}
 }
