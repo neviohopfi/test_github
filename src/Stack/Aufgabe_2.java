9f06d5c9033fd05c345b001e495834fa8c63f2ea
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-04-23 08:25:41 +0200

    finished

--- src/Stack/Aufgabe_2.java
+++ src/Stack/Aufgabe_2.java
@@ -1,10 +1,34 @@
 package Stack;
 
+import java.util.LinkedList;
+
 public class Aufgabe_2 {
 
+	public static boolean checkBrackets(String input) { //Static => kein Objekt benötigt, um sie aufzurufen
+		LinkedList<Character> stack = new LinkedList<>();
+		for(char c : input.toCharArray()) {
+			if(c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			} else if(c == ')' || c == '}' || c == ']') {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char lastOpen = stack.peek();
+				if((c == ')' && lastOpen != '(') || 
+					(c == ']' && lastOpen != '[') || 
+					(c == '}' && lastOpen != '{')) {
+					return false;
+				}
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
+	}
+	
+	
 	public static void main(String[] args) {
-		
-
+		System.out.println(checkBrackets("[{}]"));
+		System.out.println(checkBrackets("[)}](}"));
 	}
 
 }
