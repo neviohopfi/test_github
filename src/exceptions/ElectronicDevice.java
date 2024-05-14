49fa443b21a868c0ddeb73394dc57f8e409a5ff3
neviohopfi <Nevio.Hopfgartner@hak-reutte.ac.at>
2024-05-14 13:32:03 +0200

    tasks

---
+++ src/exceptions/ElectronicDevice.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class ElectronicDevice {
+
+	private String name;
+	private int watt;
+	
+	public ElectronicDevice() {}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWatt() {
+		return watt;
+	}
+	public void setWatt(int watt) {
+		if(watt > 0) {
+			this.watt = watt;
+		} else {
+			throw new IllegalWattExxception();
+		}
+	}
+	
+}
---
+++ src/exceptions/ElectronicDeviceTest.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class ElectronicDeviceTest {
+
+	public static void main(String[] args) {
+		
+		ElectronicDevice ed = new ElectronicDevice();
+		ed.setName("Radio");
+		ed.setWatt(100);
+
+	}
+
+}
